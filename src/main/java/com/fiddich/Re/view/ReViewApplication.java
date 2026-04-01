de1e10d2f52ab6bb96372ae322b6a42b05b1ee6e
Fiddich <78968690+Fiddich-Dev@users.noreply.github.com>
2026-04-01 09:38:26 +0900

    init

---
+++ src/main/java/com/fiddich/Re/view/ReViewApplication.java
@@ -0,0 +1,13 @@
+package com.fiddich.Re.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiddich/Re/view/ReViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiddich.Re.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
